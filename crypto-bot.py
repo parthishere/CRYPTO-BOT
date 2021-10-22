c4b9872197b7a923c9ac2f81ba1bf0ead719df80
parthishere <parthishere1234@gmail.com>
2021-10-22 21:36:14 +0530

    feat: change

--- crypto-bot.py
+++ crypto-bot.py
@@ -3,6 +3,9 @@ import hashlib
 from pathlib import Path
 import os
 import dotenv
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="1234")
 
 SERVER_TIME = "https://api.hotbit.io/api/v1/server.time"
 BALANCE_QUERY = "https://api.hotbit.io/api/v1/balance.query"
@@ -15,32 +18,41 @@ STATUS_TODAY = "https://api.hotbit.io/api/v1/market.status_today"
 MARKET_24_HR = "https://api.hotbit.io/api/v1/market.status24h"
 MARKET_SUMMERY = "https://api.hotbit.io/api/v1/market.summary"
 
-BASE_DIR = Path(__file__).resolve().parent
-print(BASE_DIR)
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
-
-hashlib.md5().digest()
-SIGN = hashlib.md5()
-
-
-SECRET_KEY = os.environ['secret_key']
-API_KEY = os.environ['api_key']
-ASSETS = os.environ['assets']
-
-RAW = str("api_key={}&assets={}&secret_key={}".format(API_KEY, ASSETS, SECRET_KEY))
-SIGN.update(RAW.encode('utf-8'))
-SIGN.digest()
-
-SIGN = str(SIGN.hexdigest()).upper()
-
-PARAM2 = f"api_key={API_KEY}&assets={ASSETS}&sign={SIGN}"
-PARAMS = "api_key={}&sign={}&assets={}".format(API_KEY, SIGN, ASSETS)
-
 INPUT_LOWER_RANGE = 1.15131
 INPUT_UPPER_RANGE = 1.15129
+
+PARAMS, PARAMS2 = None, None
+
+def get_params():
+    global PARAMS, PARAMS2
+    BASE_DIR = Path(__file__).resolve().parent
+    print(BASE_DIR)
+    dotenv_file = os.path.join(BASE_DIR, ".env")
+    if os.path.isfile(dotenv_file):
+        dotenv.load_dotenv(dotenv_file)
+
+    hashlib.md5().digest()
+    SIGN = hashlib.md5()
+
+    SECRET_KEY = os.environ['secret_key']
+    API_KEY = os.environ['api_key']
+    ASSETS = os.environ['assets']
+
+    RAW = str("api_key={}&assets={}&secret_key={}".format(API_KEY, ASSETS, SECRET_KEY))
+    SIGN.update(RAW.encode('utf-8'))
+    SIGN.digest()
+
+    SIGN = str(SIGN.hexdigest()).upper()
+
+    PARAMS2 = f"api_key={API_KEY}&assets={ASSETS}&sign={SIGN}"
+    PARAMS = "api_key={}&sign={}&assets={}".format(API_KEY, SIGN, ASSETS)
+
+
+if (PARAMS or PARAMS2) is None or (PARAMS or PARAMS2 == ""):
+    print('getting params')
+    get_params()
+    
+
 
 
 
@@ -81,5 +93,5 @@ print(response.content)
 
 
 #A account status of user
-response = requests.post("https://api.hotbit.io/api/v1/balance.query", data=PARAM2)
+response = requests.post("https://api.hotbit.io/api/v1/balance.query", data=PARAMS2)
 print(response.content)
