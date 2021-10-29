2419e780b4c338584a6f7f66e54eadf6a1391439
parthishere <parthishere1234@gmail.com>
2021-10-30 02:20:56 +0530

    feat: add new logger file

---
+++ logger.py
@@ -0,0 +1,18 @@
+import logging
+
+loggers = {}
+
+
+def setup_custom_logger(name, log_level=settings.LOG_LEVEL):
+    if loggers.get(name):
+        return loggers[name]
+
+    logger = logging.getLogger(name)
+    loggers[name] = logger
+
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.setLevel(log_level)
+    logger.addHandler(handler)
+    return logger
